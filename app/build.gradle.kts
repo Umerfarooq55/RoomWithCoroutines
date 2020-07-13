19f8ba5420fffcaa70e88e9e5025dddc32d60a28
moolahmobile <vernell@moolahmobile@gmail.com>
2020-07-13 05:28:28 +0500

    Updating with Coroutines

---
+++ app/build.gradle.kts
@@ -0,0 +1,49 @@
+// We are using kotlin dsl here , BuildPlugins , Android SDK and Libraries class is implemented
+// WE are using kotlin here because it can be readeable for everyone
+plugins {
+  id(BuildPlugins.androidApplication)
+  kotlin(BuildPlugins.kotlinAndroid)
+  kotlin(BuildPlugins.kotlinAndroidExtensions)
+  kotlin(BuildPlugins.kotlinKapt)
+}
+
+android {
+  compileSdkVersion(AndroidSdk.compile)
+
+  defaultConfig {
+    applicationId = "com.raywenderlich.android.rwandroidtutorial"
+    minSdkVersion(AndroidSdk.min)
+    targetSdkVersion(AndroidSdk.target)
+    versionCode = 1
+    versionName = "1.0"
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+
+  buildTypes {
+    getByName("release") {
+      isMinifyEnabled = false
+      proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+    }
+  }
+}
+
+dependencies {
+  implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+
+  // Kotlin
+  implementation(Libraries.kotlinStdLib)
+
+  // Support Libraries
+  implementation(Libraries.appCompat)
+  implementation(Libraries.recyclerView)
+  implementation(Libraries.constraintLayout)
+  implementation(Libraries.picasso)
+  implementation(Libraries.gson)
+
+  // TUTORIAL DEPENDENCIES
+  implementation(Libraries.roomRuntime)
+  kapt(Libraries.roomCompiler)
+  implementation(Libraries.roomKtx)
+  implementation(Libraries.lifecycleViewModelKtx)
+  implementation(Libraries.lifecycleExtensions)
+}
